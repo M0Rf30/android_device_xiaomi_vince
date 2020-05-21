165a139292986c08a0f094dbbf87ee7ce512ab01
Gianluca Boiano <gianluca.boiano@zextras.com>
2020-05-21 16:29:29 +0200

    Haliumify...

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -31,7 +31,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ROM_ROOT="${MY_DIR}"/../../..
 
-HELPER="${ROM_ROOT}"/vendor/aosp/build/tools/extract_utils.sh"
+HELPER="${ROM_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -44,7 +44,7 @@ setup_vendor "$DEVICE" "$VENDOR" "${ROM_ROOT}"
 # Copyright headers and guards
 write_headers
 
-write_makefiles "${MY_DIR}"/proprietary-files.txt" true
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 cat << EOF >> "$ANDROIDMK"
 
